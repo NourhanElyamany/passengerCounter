447d6d29e0a3658094d25089d1e2fc53081950bc
Nourhan <nourhanelyamany@gmail.com>
2021-07-24 00:15:44 +0200

    edits

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ let count = 0;
 let counter = document.getElementById("count");
 let save = document.getElementById("saved");
 
-function del(){
-    let countStr = counter.textContent + " | ";
-    save.textContent += countStr
-    counter.textContent = 0;
-    count = 0;
-}
-console.log(del());
 function increment(){
     count = count + 1;
     counter.textContent = count;
     console.log(counter.innerText);
+    
+}
 
+function del(){
+    let countStr = count + " | ";
+    save.textContent += countStr
+    counter.textContent = 0;
+    count = 0;
 }
